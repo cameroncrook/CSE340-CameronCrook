62f1fc2cdf610e28f22bb778bd32b6fd8ad95bac
cameroncrook <cameronbcrook@gmail.com>
2024-02-16 09:36:56 -0700

    W06 assignments

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
+const baseController = require("./controllers/baseController")
 
 app.set("view engine", "ejs")
 app.use(expressLayouts)
@@ -21,9 +23,13 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+// app.get("/", function(req, res){
+//   res.render("index", {title: "Home"})
+// })
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
